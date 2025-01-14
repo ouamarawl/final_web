798922ba4a0d007f0dcf2a857aca30c7a1c0a2a2
ouamarawl <ouamara.wail8@gmail.com>
2025-01-14 10:40:38 +0100

    register

---
+++ src/component/registre/Registre.js
@@ -0,0 +1,68 @@
+import './registre.css'
+import React, { useRef } from 'react';
+function Registre() {
+      const containerRef = useRef(null);
+    
+      const handleRegisterClick = () => {
+        containerRef.current.classList.add("active");
+      };
+    
+      const handleLoginClick = () => {
+        containerRef.current.classList.remove("active");
+      };
+  return (
+    <div className="container" ref={containerRef}>
+      <div className="form-container sign-up">
+        <form>
+          <h1>Create Account</h1>
+          <div className="social-icons">        
+            <a href="#" className="icon"><i className="fa-brands fa-google"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-linkedin"></i></a>
+
+          </div>
+          <span>or use your email for registration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+
+      <div className="form-container sign-in">
+        <form>
+          <h1>Sign In</h1>
+          <div className="social-icons">
+            <a href="#" className="icon"><i className="fa-brands fa-google"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-linkedin"></i></a>
+          </div>
+          <span>or use your email and password</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot your password?</a>
+          <button>Sign In</button>
+        </form>
+      </div>
+
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>Enter your personal details to use all of the site's features</p>
+            <button className="hidden" onClick={handleLoginClick}>Sign In</button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>Register with your personal details to use all of the site's features</p>
+            <button className="hidden" onClick={handleRegisterClick}>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Registre
